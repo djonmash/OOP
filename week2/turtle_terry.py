ecfd1f5fb98c2f121621f4f5539b4912a03f8074
John Macharia Macharia NJENGA <519447@learning.eynesbury.edu.au>
2024-02-29 12:04:58 +1030

    example

---
+++ week2/turtle_terry.py
@@ -0,0 +1,36 @@
+import turtle
+
+myTurtle = turtle.Turtle()
+myWindow = turtle.Screen()
+
+
+myTurtle = turtle.Turtle()
+myWindow = turtle.Screen()
+myWindow.setup(width=600, height=500)
+myWindow.bgcolor('skyblue')
+myTurtle.pensize(3)
+myTurtle.shape('turtle')
+myTurtle.color('white')
+myTurtle.penup()
+myTurtle.goto(50, 100)
+myTurtle.pendown()
+myTurtle.forward(50)
+myTurtle.right(90)
+myTurtle.forward(100)
+
+myTurtle.left(90) 
+myTurtle.forward(50)
+myTurtle.right(90)
+myTurtle.forward(50)
+myTurtle.right(90)
+myTurtle.forward(150)
+myTurtle.right(90)
+myTurtle.forward(50)
+myTurtle.right(90)
+myTurtle.forward(50)
+myTurtle.left(90)
+myTurtle.forward(100)
+
+
+
+myWindow.exitonclick()
